89f834a8dec8be9a79b3c0db175945f940b49904
xkolsha <aviad85@outlook.com>
2023-07-10 15:43:03 -0300

    added q for shapes, color exp to file

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 import fs from "fs";
 import inquirer from "inquirer";
 import { Triangle, Circle, Square } from "./lib/shapes.js";
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "shape",
+      message: "Which shape do you want to create?",
+      choices: ["Triangle", "Circle", "Square"],
+    },
+    {
+      type: "input",
+      name: "color",
+      message: "Enter a color for the shape:",
+    },
+  ])
+  .then((answers) => {
+    // Create the appropriate shape based on the user's choice
+    let shape;
+    switch (answers.shape) {
+      case "Triangle":
+        shape = new Triangle(answers.color);
+        break;
+      case "Circle":
+        shape = new Circle(answers.color);
+        break;
+      case "Square":
+        shape = new Square(answers.color);
+        break;
+    }
+
+    // Generate the SVG string
+    const svgString = `<svg>${shape.render()}</svg>`;
+    console.log(svgString);
+
+    // Write the SVG string to a file
+    fs.writeFileSync("examples/logo.svg", svgString);
+  });
